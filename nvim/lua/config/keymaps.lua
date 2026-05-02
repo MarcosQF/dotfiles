ddf04f69379ade9a23921058bbfc7ac647665d77
Marcos Queiroz <77897344+MarcosQF@users.noreply.github.com>
2026-05-02 19:48:09 -0300

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
+
 vim.keymap.set({ "n", "v" }, "gh", "^", { desc = "Início da linha" })
 vim.keymap.set({ "n", "v" }, "gl", "g_", { desc = "Fim da linha" })
+
+vim.keymap.set("n", "M", "'", { desc = "Marks" })
